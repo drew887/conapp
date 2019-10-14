3e72ac5f88c1c9ade24e5d91a1fcc9ccded4b022
Andrew Mcdonald <drew887121@gmail.com>
2019-10-13 23:13:00 -0400

    Downloads are made into repo dir now

--- swat.py
+++ swat.py
@@ -6,18 +6,29 @@ from swat.arguments import validate_args, get_args
 from swat.url_generators import RESOLVERS
 from swat.file_paths import *
 
+
 def main(args: argparse.Namespace) -> None:
     if not validate_args(args):
         exit(-1)
 
-    if not check_dirs():
+    if not check_dirs(args.user, args.repo):
         print("creating config dirs...")
-        create_dirs()
-    print(RESOLVERS.get(args.host)(args.user, args.repo))
+        create_dirs(args.user, args.repo)
 
-    urllib.request.urlretrieve(RESOLVERS.get(args.host)(args.user, args.repo), f'{args.user}.{args.repo}.tar.gz')
+    url = RESOLVERS.get(args.host)(args.user, args.repo)
+    file_name = get_repo_dir(args.user, args.repo) + "/" + f"{args.user}.{args.repo}.tar.gz"
+
+    download_file(file_name, url)
 
 
+def download_file(file_name: str, url: str) -> None:
+    try:
+        print(f"Attempting to download {url}")
+        urllib.request.urlretrieve(url, file_name)
+        print(f"Success, downloaded to {file_name}")
+    except urllib.request.HTTPError as ex:
+        print(f"Error occurred, does {url} exist?\n{ex}")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main(get_args())
--- swat/file_paths.py
+++ swat/file_paths.py
@@ -1,26 +1,41 @@
 import os
 
 CONFIG_DIR = os.environ.get('XDG_CONFIG_HOME', os.path.expanduser("~/")) + ".config/swat"
-
-CONFIG_DIRS = [
-    "snapshots",
-    "repo"
-]
+CONFIG_DIR_REPO = "repo"
+CONFIG_DIR_SNAPSHOT = "snapshots"
 
 
 def get_config_dir(dir: str) -> str:
     return CONFIG_DIR + '/' + dir
 
 
-def check_dirs() -> bool:
-    result = False
-
-    for conf_dir in CONFIG_DIRS:
-        result = result or os.path.isdir(get_config_dir(conf_dir))
-
-    return result
+CONFIG_DIRS = [
+    get_config_dir(CONFIG_DIR_SNAPSHOT),
+    get_config_dir(CONFIG_DIR_REPO)
+]
 
 
-def create_dirs() -> None:
-    for config_dir in CONFIG_DIRS:
-        os.makedirs(get_config_dir(config_dir))
+# TODO: Abstract repo default into a constant somewhere
+def get_repo_dir(user: str, repo: str) -> str:
+    return get_config_dir(CONFIG_DIR_REPO) + "/" + user + "/" + repo
+
+
+def check_dirs(user: str, repo: str) -> bool:
+    user_dir = get_repo_dir(user, repo)
+
+    for config_dir in CONFIG_DIRS + [user_dir]:
+        if not os.path.isdir(config_dir):
+            return False
+
+    return True
+
+
+def create_dirs(user: str, repo: str) -> None:
+    user_dir = get_repo_dir(user, repo)
+
+    for config_dir in CONFIG_DIRS + [user_dir]:
+        try:
+            os.makedirs(config_dir)
+        except FileExistsError:
+            print(f"{config_dir} already exists, not recreating")
+            continue
