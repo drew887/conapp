cf06e5947dc49920fd6bbf51000ed2b843595bc6
Andrew Mcdonald <drew887121@gmail.com>
2019-10-31 21:51:18 -0400

    v0.1.0 The "MVP"
    I believe we finally have a usable mvp kind of situation

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="conapp",
-    version="0.0.5",
+    version="0.1.0",
     author="drew887 <Andrew Mcdonald>",
     author_email="drew887121@gmail.com",
     description="A simple config applier",
