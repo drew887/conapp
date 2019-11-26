547e880df11f5283e66cb69e226e10370d312db1
Andrew Mcdonald <drew887121@gmail.com>
2019-11-25 23:13:43 -0500

    Flush out MVP for track add command

--- conapp/commands/track.py
+++ conapp/commands/track.py
@@ -1,6 +1,8 @@
 import os
 import argparse
 
+from conapp.file_paths import CONFIG_TRACK_DIR
+
 COMMAND = "track"
 COMMAND_HELP = "Command for managing a local repo in the format that conapp expects"
 
@@ -19,6 +21,11 @@ def setup_arguments(parser: argparse.ArgumentParser) -> argparse.ArgumentParser:
         "file",
         help="File to start tracking"
     )
+    add_parser.add_argument(
+        "--force",
+        action="store_true",
+        help="Overwrite if already tracked"
+    )
 
     info_parser = sub_parser.add_parser(
         "info",
@@ -35,17 +42,39 @@ def add_command(args: argparse.Namespace) -> None:
     :param args:
     :return:
     """
-    if not os.path.exists(args.file):
-        print(f"Error {args.file} doesn't exist")
+    if not os.path.exists(args.file) or not os.path.isfile(args.file):
+        print(f"Error {args.file} doesn't exist or isn't a file")
+        print("conapp currently doesn't track folders")
 
     abs_path = os.path.abspath(args.file)
     home_path = os.path.expanduser("~")
-    print(abs_path)
 
     if abs_path.startswith(home_path):
-        relative_home_path = abs_path[len(home_path):]
+        # we gotta chop off the leading '/' as well
+        relative_home_path = abs_path[(len(home_path) + 1):]
+        abs_track_path = os.path.join(CONFIG_TRACK_DIR, relative_home_path)
 
-        print(relative_home_path)
+        if args.force or not os.path.exists(abs_track_path):
+            if input(f"track {abs_track_path} ? [y/N]: ") == 'y':
+                # create any leading folders needed
+                # TODO: add flag to disable this and just add at top level
+                leading_track_folder = abs_track_path[0:abs_track_path.rfind("/")]
+
+                if not os.path.exists(leading_track_folder):
+                    os.makedirs(leading_track_folder)
+
+                # TODO: Add extra flags to control how file makes it into repo
+
+                if args.force and os.path.exists(abs_track_path):
+                    print("Removing old file")
+                    os.remove(abs_track_path)
+
+                print("Tracking file via hardlink")
+                os.link(abs_path, abs_track_path)
+            else:
+                print("Not tracking file")
+        else:
+            print("File already tracked")
 
     pass
 
